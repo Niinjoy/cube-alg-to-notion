2131f4aba12962542d45836726bd6c18f50ee3e8
Niinjoy <39721307+Niinjoy@users.noreply.github.com>
2021-07-02 16:21:24 +0800

    add fave data to Notion

---
+++ getFave.js
@@ -0,0 +1,33 @@
+const fs = require("fs")
+const util = require('util')
+const request = require("request");
+const cheerio = require('cheerio')
+
+const requestPromise = util.promisify(request);
+const speedCubeId = "1952"
+const algsets = ["F2L", "OLL", "PLL"]
+const algLost = ["U R U' R'", "F' r U r'", "U L U' L'", "U f R' f'"] // F2L 1 won't show complete, so add manually
+const faveJsonDir = __dirname+"/asset/algFave.json"
+let algFave = []
+
+async function main() {
+  algFave.push.apply(algFave,(algLost.map(alg => { return {algset: "F2L", alg: alg }})))
+  for (algset of algsets) {
+    await getAlgFave(algset)
+  }
+  fs.writeFileSync(faveJsonDir, JSON.stringify(algFave, null, "  "))
+}
+
+async function getAlgFave(algset) {
+  const url = "https://www.speedcubedb.com/algsheet/" + speedCubeId + "/" + algset
+  const response = await requestPromise(url)
+  const data = cheerio.load(response.body)
+  var algs = []
+  data("span").each(function(i, elem) {
+    algs[i] = data(this).text()
+  })
+  algs = algs.filter(i => i && !algLost.includes(i)) // delete empty element and element in algLost
+  algFave.push.apply(algFave,(algs.map(alg => { return {algset: algset, alg: alg }})))
+}
+
+main()
